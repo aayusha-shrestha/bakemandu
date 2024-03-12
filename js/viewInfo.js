27ee9ce172233072eba1d65d86f628a8a0bcee9d
Aayusha <aayyushashrestha@gmail.com>
2024-03-12 23:54:37 +0545

    final

--- js/viewInfo.js
+++ js/viewInfo.js
@@ -1,15 +1,15 @@
-var infoTitle = document.getElementsByClassName('infoTitle');
-var infoContent = document.getElementsByClassName('infoContent');
+var tabTitle = document.getElementsByClassName('tab__title');
+var tabContent = document.getElementsByClassName('tab__content');
 
 function viewInfo(titleName) {
-    for(title of infoTitle) {
-        title.classList.remove("activeTitle");
-        if (title.classList.contains(titleName)) {
-            title.classList.add("activeTitle");
+    for(title of tabTitle) {
+        title.classList.remove("tab__title--active");
+        if (title.classList.contains('tab__title--' + titleName)) {
+            title.classList.add("tab__title--active");
         }
     }
-    for(content of infoContent) {
-        content.classList.remove("activeContent");
+    for(content of tabContent) {
+        content.classList.remove("tab__content--active");
     }
-    document.getElementById(titleName).classList.add("activeContent");
+    document.getElementById(titleName).classList.add("tab__content--active");
 }
