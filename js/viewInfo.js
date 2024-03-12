7ab528d269fce6128014d1afe7fcb7587964302e
Aayusha <aayyushashrestha@gmail.com>
2024-03-12 21:34:25 +0545

    completed hamburger menu

--- js/viewInfo.js
+++ js/viewInfo.js
@@ -1,8 +1,12 @@
 var infoTitle = document.getElementsByClassName('infoTitle');
 var infoContent = document.getElementsByClassName('infoContent');
+
 function viewInfo(titleName) {
     for(title of infoTitle) {
         title.classList.remove("activeTitle");
+        if (title.classList.contains(titleName)) {
+            title.classList.add("activeTitle");
+        }
     }
     for(content of infoContent) {
         content.classList.remove("activeContent");
