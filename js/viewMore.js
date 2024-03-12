3fb029696fa70118707c2d7d7768d7d8a5efcdcb
Aayusha <aayyushashrestha@gmail.com>
2024-03-12 23:49:44 +0545

    view more js

--- js/viewMore.js
+++ js/viewMore.js
@@ -1,7 +1,4 @@
 function viewMore() {
     document.getElementById("view-more-items").style.display = "flex";
-    var viewMoreContainer = document.getElementsByClassName("view-more-container");
-    for(var i = 0; i < viewMoreContainer.length; i++) {
-        viewMoreContainer[i].style.display = "none";
-    }
+    document.querySelector(".view-more-container").style.display = "none";
 }
